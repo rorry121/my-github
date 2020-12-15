f35a38899e9ec5ee6d79da3e08fd15d89cd77478
rorry <luolei@haizhi.com>
2020-12-15 17:29:20 +0800

    test: change test item

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-github'`, () => {
+  it(`should have as title 'PPPP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-github');
+    expect(app.title).toEqual('PPPP');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-github app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('PPPP app is running!');
   });
 });
